96712b4574f79f483ca0ae09cc2e1c816c41d7ba
chungoloaf <69054513+chungoloaf@users.noreply.github.com>
2023-01-11 17:28:18 -0500

    Update server.c
    Added a cpuinfo function that will allow you to aks the server for CPU info and get it

--- server.c
+++ server.c
@@ -15,6 +15,7 @@
 #include <unistd.h>
 #include <errno.h>
 
+
 #endif
 
 #if defined(_WIN32)
@@ -43,6 +44,26 @@
 
 
 int BUFFER=1024, waitlist=15;
+int msleep(long tms)
+{
+    struct timespec ts;
+    int ret;
+
+    if (tms < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = tms / 1000;
+    ts.tv_nsec = (tms % 1000) * 1000000;
+
+    do {
+        ret = nanosleep(&ts, &ts);
+    } while (ret && errno == EINTR);
+
+    return ret;
+}
 
 struct socket_params{
     int socket_number;
@@ -51,12 +72,71 @@ struct socket_params{
     char* message_buffer;
     SOCKET my_max_socket;
     fd_set master;
+    char ** my_address;
 };
 
 
+void getcpuinfo(void* context){
+    struct socket_params *struct_ptr=(struct socket_params*) context;
+    char c;
+    FILE *fptr;
+    int MAX_LINES= 500;
+    int  MAX_LEN= 1000;
 
-////TO DO!!!!!
-/*Post to github and make it show the ip address of each client it you van if not just do socket numbers*/
+
+    char ** cpudata=malloc(sizeof(char*)*MAX_LINES);
+    for(int x=0; x<MAX_LINES ; x++){
+        cpudata[x]=malloc(sizeof(char)*MAX_LEN);
+    }
+
+    fptr= fopen("/proc/cpuinfo", "r");
+    if (fptr == NULL){
+        printf("Error opening file \n");
+    }
+    
+    else{
+       
+        int line=0;
+        while (!feof(fptr) && !ferror(fptr))
+    {
+        if(fgets(cpudata[line], MAX_LEN, fptr) != NULL){
+            line++;
+        }
+        
+        
+    }
+    
+    int *lineptr= &line;
+    SOCKET j;
+    printf("Sending CPU information to client %d \n", struct_ptr->socket_number);
+
+                   for (j = 1; j <= struct_ptr->my_max_socket; ++j) {
+                        if (FD_ISSET(j, &struct_ptr->master)) {
+                            if ( j == struct_ptr->socket_number){
+                                send(j, &line, sizeof(line),0);
+                                for(int x=0; x<line; x++){
+                                    send(j, cpudata[x], MAX_LEN,0);
+                                    msleep(10*7 );}
+                                    
+                                }
+                            else
+                                 continue;
+                               }
+}         
+    }
+
+                
+
+
+    for (int x =0; x<MAX_LINES; x++){
+        free(cpudata[x]);
+        cpudata[x]=NULL;
+    }
+    free(cpudata);
+    cpudata=NULL;
+    fclose(fptr);
+}
+
 
 void *message_handler(void* context){
     
@@ -92,15 +172,15 @@ void *fd_handler(void* context){
     
                     FD_SET(socket_client,&struct_ptr->master);
                     if (socket_client > struct_ptr->my_max_socket)
-                    struct_ptr->my_max_socket = socket_client;
+                        struct_ptr->my_max_socket = socket_client;
                     
                     char *address_buffer=calloc(BUFFER,sizeof(char));
                     getnameinfo((struct sockaddr*)&client_address,
                             client_len,
                             address_buffer, BUFFER, 0, 0,
                             NI_NUMERICHOST);
-                    printf("New connection from %s its its client number is %d \n", address_buffer, struct_ptr->socket_number);
-                    free(address_buffer);
+                    printf("New connection from %s its its client number is %d \n", address_buffer, socket_client);
+                    struct_ptr->my_address=&address_buffer;
                     
             return NULL;
 
@@ -182,39 +262,45 @@ struct socket_params my_socket;
                 if (i == socket_listen) {
                     if(pthread_create(&fd_thread[i], NULL, &fd_handler,
                     (void *)(intptr_t)&my_socket)==0){
-                        fprintf(stderr,"Connection Handler Started \n");
+                        //fprintf(stderr,"Connection Handler Started \n");
                 }
-                pthread_join(fd_thread[i],NULL);
+                char *my_address=calloc(BUFFER,sizeof(char));
+                pthread_join(fd_thread[i], NULL);
+
                 }
             
-                    
-\
-
 
                  else { // this is for clients send messages it will multithread it
                    char* read=calloc(BUFFER,sizeof(char));
                     my_socket.message_buffer=read;
                     my_socket.message_bytes = recv(i, read, BUFFER, 0);
                     read=realloc(read,strlen(read));
+                    
                   
                     printf("Client %d message is: %s \n",i, my_socket.message_buffer);
-                    if(pthread_create(&message_thread[i], NULL, &message_handler,
-                    (void *)(intptr_t)&my_socket)==0){
-                        fprintf(stderr,"Message Handler Started \n");
-                }
-                pthread_join(message_thread[i],NULL);
+                        if(strcmp(my_socket.message_buffer, "cpuinfo\n")==0){
+                            getcpuinfo(&my_socket);}
+                            
+                    
+                        else{
+                        if(pthread_create(&message_thread[i], NULL, &message_handler,
+                        (void *)(intptr_t)&my_socket)==0){
+                            //fprintf(stderr,"Message Handler Started \n");
+                            pthread_join(message_thread[i],NULL);
+                            }    
+                        }
 
-                    if (my_socket.message_bytes < 1) {
-                        printf("Closing connection from %d \n", i);
-                        FD_CLR(i, &my_socket.master);
-                        CLOSESOCKET(i);
-                        continue;
+                        if (my_socket.message_bytes < 1) {
+                            printf("Closing connection from %d \n", i);
+                            FD_CLR(i, &my_socket.master);
+                            CLOSESOCKET(i);
+                            continue;
+                        }
+                        free(read);
+                        
+                        
+                        
                     }
-                    
-                    
-                    free(read);
-                    
-                }
             } //if FD_ISSET
         } //for i to max_socket
     } //while(1)
